9084e23e2eb83953decceaddd1cebbeaf6bc8aac
emanherawy <eman.herawy@gmail.com>
2020-04-11 01:46:28 +0200

    display messages for the end user

--- src/index.js
+++ src/index.js
@@ -18,15 +18,24 @@ const serviceGen = async ({contractsBuildDir, provider} ) => {
    const service_path = path.resolve(path.join(contractsBuildDir,'../','../', 'service'));
 
    const dirFiles = await fse.readdir(contractsBuildDir)
+   console.log(`generating web3 functions in ${service_path} for contract artifacts in  ${dirFiles}`);
+
    if(!await fse.pathExists(service_path)){
+      console.log(`creating service folder in  ${service_path}`);
+
       await fse.mkdirSync(service_path)
    }
    const outputDir =  service_path;
    dirFiles.map(async (file) => {
-
+         console.log(` reading ${contractsBuildDir}/${file} artifacts ...`);
+         
       const contract = await readArtifact(`${contractsBuildDir}/${file}`);
       const isDeployed = Object.keys(contract.networks).length === 0 && contract.networks.constructor === Object;
-       const status = await generateFun(contract, provider, outputDir, contractsBuildDir, !isDeployed);
+      console.log(` contract ${contractsBuildDir}/${file} migration status is ${isDeployed}`);
+
+      const status = await generateFun(contract, provider, outputDir, contractsBuildDir, !isDeployed);
+      console.log(`status ${status}`);
+
 
    })
 
